d0efaa4b64792162c14b4751e924f07c07cfbdfb
onedreameach <onedreameach@gmail.com>
2026-08-11 23:35:13 +0200

    Update wall.js

--- api/wall.js
+++ api/wall.js
@@ -1,9 +1,25 @@
 module.exports = function handler(req, res) {
+try {
+var baseUrl = process.env.SUPABASE_URL;
+var key = process.env.SUPABASE_ANON_KEY;
+
+```
+var endpoint =
+  baseUrl +
+  "/rest/v1/Dreams?select=id,dream_number,nickname,dream_text,country,instagram,tiktok,created_at&order=dream_number.asc";
+
 return res.status(200).json({
-require_type: typeof require,
-https_type: typeof require("https"),
-https_get_type: typeof require("https").get,
-res_status_type: typeof res.status,
-res_json_type: typeof res.json
+  test: "URL OK",
+  url_type: typeof endpoint,
+  url_start: endpoint.substring(0, 30),
+  key_type: typeof key
 });
+```
+
+} catch (error) {
+return res.status(500).json({
+error: "URL TEST ERROR",
+details: error.message
+});
+}
 };
