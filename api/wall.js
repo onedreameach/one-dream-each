20c8b447625108be7cabbd13f8f36e2a7e10cdcc
onedreameach <onedreameach@gmail.com>
2026-08-11 23:13:01 +0200

    Update wall.js

--- api/wall.js
+++ api/wall.js
@@ -1,37 +1,6 @@
 export default async function handler(req, res) {
-  try {
-    const url =
-      process.env.SUPABASE_URL +
-      "/rest/v1/Dreams?select=id,dream_number,nickname,dream_text,country,instagram,tiktok,created_at&order=dream_number.asc";
-
-    const response = await fetch(url, {
-      headers: {
-        apikey: process.env.SUPABASE_ANON_KEY,
-        Authorization: "Bearer " + process.env.SUPABASE_ANON_KEY,
-      },
-    });
-
-    const responseText = await response.text();
-
-    if (!response.ok) {
-      return res.status(500).json({
-        error: "Supabase request failed",
-        status: response.status,
-        details: responseText,
-      });
-    }
-
-    const dreams = JSON.parse(responseText);
-
-    return res.status(200).json({
-      count: dreams.length,
-      dreams: dreams,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      error: "Unable to load dreams",
-      details: error.message,
-    });
-  }
+  return res.status(200).json({
+    count: 0,
+    dreams: []
+  });
 }
-```
